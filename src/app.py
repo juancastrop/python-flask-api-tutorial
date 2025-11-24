a2ddaa5aababd7eb291ec51dd7a6ffe1863ceabe
juan <130606010+juancastrop@users.noreply.github.com>
2025-11-24 21:45:47 +0000

    avanzando en ejercicios7.2

--- src/app.py
+++ src/app.py
@@ -1,2 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask import request
 app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
